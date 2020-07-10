41dd7def5ddd5f2ca0c02a5b88c0fabb9436a719
Lizzy Presland <lizzy.presland@gmail.com>
2020-07-10 13:40:54 -0700

    Delete ignored CMake files

--- CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/chris/OneDrive/Desktop/SummerOfBrain
-# Build directory: /mnt/c/Users/chris/OneDrive/Desktop/SummerOfBrain
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
