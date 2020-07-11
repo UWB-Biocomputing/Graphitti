c8b7414246e8745d428148a45b7a499d12dfce12
Lizzy Kate <lizzy.presland@gmail.com>
2020-07-11 11:56:43 -0700

    Ignore common IDE and system files & dirs (#30)
    * Ignore common IDE and system files & dirs
    
    Not a complete list by any means, but should take care of common
    use cases (and current problematic directories).
    
    * Delete ignored CMake files

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/chris/OneDrive/Desktop/SummerOfBrain
-# Build directory: /mnt/c/users/chris/onedrive/desktop/summerofbrain/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("Testing/lib/googletest-master")
