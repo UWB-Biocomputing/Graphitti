66a825cc71a7e52428d215be285344a8b1396116
christopherdokeefe <cdok@uw.edu>
2020-09-07 15:03:50 -0700

    Update BGDriver.cpp

--- Simulation/Core/BGDriver.cpp
+++ Simulation/Core/BGDriver.cpp
@@ -2,13 +2,14 @@
  * @file Driver
  *
  *  The driver performs the following steps:
- *  1) Parses command line to get configuration file and additional information if provided
- *  2) Instantiates Simulator and loads its parameters from config file
- *  3) Instatiates all simulator objects
- *  4) Reads parameters from an configuration file
- *  5) Simulation setup (Deseralization, Initailizing values, etc.)
- *  6) Run Simulation
- *  7) Simulation shutdown (Save results, serialize)
+ *  1) Instantiates Simulator object
+ *  2) Parses command line to get configuration file and additional information if provided
+ *  3) Loads global Simulator parameters from configuration file
+ *  4) Instantiates all simulator objects (Layout, Connections, Synapases, Neurons)
+ *  5) Reads simulator objects' parameters from configuration file
+ *  6) Simulation setup (Deseralization, Initailizing values, etc.)
+ *  7) Run Simulation
+ *  8) Simulation shutdown (Save results, serialize)
  *
  */
 
