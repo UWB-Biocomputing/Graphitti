d650a2f2ae61d1cde68bb988cb8776b1616e5624
Kyle Dukart <46544456+kyledukart@users.noreply.github.com>
2021-01-10 14:16:27 -0800

    update connections.h comments

--- Simulation/Connections/Connections.h
+++ Simulation/Connections/Connections.h
@@ -1,10 +1,9 @@
 /**
- *      @file Connections.h
+ * @file Connections.h
+ * 
+ * @ingroup Simulation/Connections
  *
- *      @brief The base class of all connections classes
- */
-
-/**
+ * @brief The base class of all connections classes
  *
  * @class Connections Connections.h "Connections.h"
  *
@@ -54,92 +53,92 @@ class Connections {
 public:
    Connections();
 
-   /**
-    *  Destructor
-    */
+   ///
+   ///  Destructor
+   ///
    virtual ~Connections();
 
-   /**
-    * Returns shared pointer to Synapses/Edges 
-    */
+   ///
+   /// Returns shared pointer to Synapses/Edges 
+   ///
    shared_ptr<IAllSynapses> getSynapses() const;
 
 
-   /**
-    * Returns a shared pointer to the SynapseIndexMap
-    */
+   ///
+   /// Returns a shared pointer to the SynapseIndexMap
+   ///
    shared_ptr<SynapseIndexMap> getSynapseIndexMap() const;
 
-   /**
-    * Calls Synapses to create SynapseIndexMap and stores it as a member variable
-    */
+   ///
+   /// Calls Synapses to create SynapseIndexMap and stores it as a member variable
+   ///
    void createSynapseIndexMap();
 
-   /**
-    *  Setup the internal structure of the class (allocate memories and initialize them).
-    *
-    *  @param  layout    Layout information of the neunal network.
-    *  @param  neurons   The Neuron list to search from.
-    *  @param  synapses  The Synapse list to search from.
-    */
+   ///
+   ///  Setup the internal structure of the class (allocate memories and initialize them).
+   ///
+   ///  @param  layout    Layout information of the neunal network.
+   ///  @param  neurons   The Neuron list to search from.
+   ///  @param  synapses  The Synapse list to search from.
+   ///
    virtual void setupConnections(Layout *layout, IAllNeurons *neurons, IAllSynapses *synapses) = 0;
 
-   /*
-    * Load member variables from configuration file.
-    * Registered to OperationManager as Operations::op::loadParameters
-    */
+   ///
+   /// Load member variables from configuration file.
+   /// Registered to OperationManager as Operations::op::loadParameters
+   ///
    virtual void loadParameters() = 0;
 
-   /**
-   *  Prints out all parameters to logging file.
-   *  Registered to OperationManager as Operation::printParameters
-   */
+   ///
+   ///  Prints out all parameters to logging file.
+   ///  Registered to OperationManager as Operation::printParameters
+   ///
    virtual void printParameters() const = 0;
 
-   /**
-    *  Update the connections status in every epoch.
-    *
-    *  @param  neurons  The Neuron list to search from.
-    *  @param  layout   Layout information of the neunal network.
-    *  @return true if successful, false otherwise.
-    */
+   ///
+   ///  Update the connections status in every epoch.
+   ///
+   ///  @param  neurons  The Neuron list to search from.
+   ///  @param  layout   Layout information of the neunal network.
+   ///  @return true if successful, false otherwise.
+   ///
    virtual bool updateConnections(IAllNeurons &neurons, Layout *layout);
 
-   /**
-    *  Creates synapses from synapse weights saved in the serialization file.
-    *
-    *  @param  numNeurons Number of neurons to update.
-    *  @param  layout      Layout information of the neunal network.
-    *  @param  ineurons    The Neuron list to search from.
-    *  @param  isynapses   The Synapse list to search from.
-    */
+   ///
+   ///  Creates synapses from synapse weights saved in the serialization file.
+   ///
+   ///  @param  numNeurons Number of neurons to update.
+   ///  @param  layout      Layout information of the neunal network.
+   ///  @param  ineurons    The Neuron list to search from.
+   ///  @param  isynapses   The Synapse list to search from.
+   ///
    void
    createSynapsesFromWeights(const int numNeurons, Layout *layout, IAllNeurons &ineurons, IAllSynapses &isynapses);
 
 #if defined(USE_GPU)
    public:
-       /**
-        *  Update the weight of the Synapses in the simulation.
-        *  Note: Platform Dependent.
-        *
-        *  @param  numNeurons          number of neurons to update.
-        *  @param  neurons             the Neuron list to search from.
-        *  @param  synapses            the Synapse list to search from.
-        *  @param  allNeuronsDevice    GPU address of the allNeurons struct on device memory.
-        *  @param  allSynapsesDevice   GPU address of the allSynapses struct on device memory.
-        *  @param  layout              Layout information of the neunal network.
-        */
+       ///
+       ///  Update the weight of the Synapses in the simulation.
+       ///  Note: Platform Dependent.
+       ///
+       ///  @param  numNeurons          number of neurons to update.
+       ///  @param  neurons             the Neuron list to search from.
+       ///  @param  synapses            the Synapse list to search from.
+       ///  @param  allNeuronsDevice    GPU address of the allNeurons struct on device memory.
+       ///  @param  allSynapsesDevice   GPU address of the allSynapses struct on device memory.
+       ///  @param  layout              Layout information of the neunal network.
+       ///
        virtual void updateSynapsesWeights(const int numNeurons, IAllNeurons &neurons, IAllSynapses &synapses, AllSpikingNeuronsDeviceProperties* allNeuronsDevice, AllSpikingSynapsesDeviceProperties* allSynapsesDevice, Layout *layout);
 #else
 public:
-   /**
-    *  Update the weight of the Synapses in the simulation.
-    *  Note: Platform Dependent.
-    *
-    *  @param  numNeurons Number of neurons to update.
-    *  @param  ineurons    The Neuron list to search from.
-    *  @param  isynapses   The Synapse list to search from.
-    */
+   ///
+   ///  Update the weight of the Synapses in the simulation.
+   ///  Note: Platform Dependent.
+   ///
+   ///  @param  numNeurons Number of neurons to update.
+   ///  @param  ineurons    The Neuron list to search from.
+   ///  @param  isynapses   The Synapse list to search from.
+   ///
    virtual void
    updateSynapsesWeights(const int numNeurons, IAllNeurons &neurons, IAllSynapses &synapses, Layout *layout);
 
