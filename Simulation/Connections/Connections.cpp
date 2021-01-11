5f4d6bb3a993f0b03ebdd5c0a7ff4c4cba8a0ac7
Kyle Dukart <46544456+kyledukart@users.noreply.github.com>
2021-01-11 12:29:52 -0800

    update formatting style

--- Simulation/Connections/Connections.cpp
+++ Simulation/Connections/Connections.cpp
@@ -53,20 +53,20 @@
 #include "EdgesFactory.h"
 
 Connections::Connections() {
-   /// Create Edges/Synapses class using type definition in configuration file
+   // Create Edges/Synapses class using type definition in configuration file
    string type;
    ParameterManager::getInstance().getStringByXpath("//SynapsesParams/@class", type);
    synapses_ = EdgesFactory::getInstance()->createEdges(type);
 
-   /// Register printParameters function as a printParameters operation in the OperationManager
+   // Register printParameters function as a printParameters operation in the OperationManager
    function<void()> printParametersFunc = bind(&Connections::printParameters, this);
    OperationManager::getInstance().registerOperation(Operations::printParameters, printParametersFunc);
 
-   /// Register loadParameters function with Operation Manager
+   // Register loadParameters function with Operation Manager
    function<void()> function = std::bind(&Connections::loadParameters, this);
    OperationManager::getInstance().registerOperation(Operations::op::loadParameters, function);
 
-   /// Get a copy of the file logger to use log4cplus macros
+   // Get a copy of the file logger to use log4cplus macros
    fileLogger_ = log4cplus::Logger::getInstance(LOG4CPLUS_TEXT("file"));
 }
 
@@ -85,13 +85,11 @@ void Connections::createSynapseIndexMap() {
    synapseIndexMap_ = shared_ptr<SynapseIndexMap>(synapses_->createSynapseIndexMap());
 }
 
-///
 ///  Update the connections status in every epoch.
 ///
 ///  @param  neurons  The Neuron list to search from.
 ///  @param  layout   Layout information of the neunal network.
 ///  @return true if successful, false otherwise.
-///
 bool Connections::updateConnections(IAllNeurons &neurons, Layout *layout) {
    return false;
 }
@@ -102,39 +100,35 @@ void Connections::updateSynapsesWeights(const int numNeurons, IAllNeurons &neuro
 }
 #else
 
-///
 ///  Update the weight of the Synapses in the simulation.
 ///  Note: Platform Dependent.
 ///
 ///  @param  numNeurons  Number of neurons to update.
 ///  @param  neurons     The Neuron list to search from.
 ///  @param  synapses    The Synapse list to search from.
-///
 void Connections::updateSynapsesWeights(const int numNeurons, IAllNeurons &neurons, IAllSynapses &synapses, Layout *layout) {
 }
 
 #endif // !USE_GPU
 
-///
 ///  Creates synapses from synapse weights saved in the serialization file.
 ///
 ///  @param  numNeurons  Number of neurons to update.
 ///  @param  layout      Layout information of the neunal network.
 ///  @param  ineurons    The Neuron list to search from.
 ///  @param  isynapses   The Synapse list to search from.
-///
 void Connections::createSynapsesFromWeights(const int numNeurons, Layout *layout, IAllNeurons &ineurons,
                                             IAllSynapses &isynapses) {
    AllNeurons &neurons = dynamic_cast<AllNeurons &>(ineurons);
    AllSynapses &synapses = dynamic_cast<AllSynapses &>(isynapses);
 
-   /// for each neuron
+   // for each neuron
    for (int iNeuron = 0; iNeuron < numNeurons; iNeuron++) {
-      /// for each synapse in the neuron
+      // for each synapse in the neuron
       for (BGSIZE synapseIndex = 0;
            synapseIndex < Simulator::getInstance().getMaxSynapsesPerNeuron(); synapseIndex++) {
          BGSIZE iSyn = Simulator::getInstance().getMaxSynapsesPerNeuron() * iNeuron + synapseIndex;
-         /// if the synapse weight is not zero (which means there is a connection), create the synapse
+         // if the synapse weight is not zero (which means there is a connection), create the synapse
          if (synapses.W_[iSyn] != 0.0) {
             BGFLOAT theW = synapses.W_[iSyn];
             BGFLOAT *sumPoint = &(neurons.summationMap_[iNeuron]);
