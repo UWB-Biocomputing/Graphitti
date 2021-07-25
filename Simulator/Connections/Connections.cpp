b0ef10699617b64528d52b7ba18f35cb619ec629
Vivek Gandhi <gandhiv@uw.edu>
2021-07-24 20:49:29 -0700

    Fixed issue #222: EdgeIndexMap was writing on dirty data

--- Simulator/Connections/Connections.cpp
+++ Simulator/Connections/Connections.cpp
@@ -61,13 +61,12 @@ void Connections::createEdgeIndexMap() {
       synapseIndexMap_ = shared_ptr<EdgeIndexMap>(new EdgeIndexMap(vertexCount, maxEdges));
    }
 
-   // Unsure if required
-   // fill_n(synapseIndexMap_->incomingEdgeBegin_, vertexCount, 0);
-   // fill_n(synapseIndexMap_->incomingEdgeCount_, vertexCount, 0);
-   // fill_n(synapseIndexMap_->incomingEdgeIndexMap_, maxEdges, 0);
-   // fill_n(synapseIndexMap_->outgoingEdgeBegin_, vertexCount, 0);
-   // fill_n(synapseIndexMap_->outgoingEdgeCount_, vertexCount, 0);
-   // fill_n(synapseIndexMap_->outgoingEdgeIndexMap_, maxEdges, 0);
+   fill_n(synapseIndexMap_->incomingEdgeBegin_, vertexCount, 0);
+   fill_n(synapseIndexMap_->incomingEdgeCount_, vertexCount, 0);
+   fill_n(synapseIndexMap_->incomingEdgeIndexMap_, maxEdges, 0);
+   fill_n(synapseIndexMap_->outgoingEdgeBegin_, vertexCount, 0);
+   fill_n(synapseIndexMap_->outgoingEdgeCount_, vertexCount, 0);
+   fill_n(synapseIndexMap_->outgoingEdgeIndexMap_, maxEdges, 0);
 
    edges_->createEdgeIndexMap(synapseIndexMap_);
 }
