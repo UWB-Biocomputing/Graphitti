65295cfe11bc828da781b2941a7f2d92adf17901
jardiamj <1088732+jardiamj@users.noreply.github.com>
2022-10-26 08:16:24 -0700

    Added subgraph extracting script

---
+++ Tools/gis2graph/get_subgraph.py
@@ -0,0 +1,22 @@
+import networkx as nx
+
+def main():
+    """Extracts the subgraph for the Seattle PD PSAP from the King County GraphML file."""
+
+    graphml_file = "graph_files/King_county_NG911.graphml"
+    edge_filter = "977" # this is the id of the SEATTLE PD vertex in the King_County graph
+    subgraph_file = "graph_files/spd.graphml"
+    G =nx.read_graphml(graphml_file)
+
+    # extract the subgraph for the given vertex
+    G_spd = nx.Graph()
+    G_spd.add_edges_from(G.edges(edge_filter, data=True))
+    for node in G_spd.nodes():
+        G_spd.nodes[node].update(G.nodes[node])
+
+    # This will give us a continuos numbering starting from zero
+    G_spd = nx.convert_node_labels_to_integers(G_spd, first_label=0, ordering='default')
+    nx.write_graphml(G_spd, subgraph_file)
+
+if __name__ == '__main__':
+    main()
