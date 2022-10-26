42925c6b71183cccf597a31532acd98c620fe112
jardiamj <1088732+jardiamj@users.noreply.github.com>
2022-10-25 18:03:53 -0700

    Made sure all vertices in the graph have a name and objectID. Converted the node's id to a continous number.

--- Tools/gis2graph/gis2graph.py
+++ Tools/gis2graph/gis2graph.py
@@ -48,6 +48,7 @@ def main():
 
     names = [] # 2 empty lists for storing names and polygons for merging psaps together
     polys = []
+    es_nguid = [] # list for storing the nguids
 
     # Loops through and finds all unique names, as well as sorting all the polygons that make up those regions
     for n in range(kc_psap.shape[0]):
@@ -56,6 +57,7 @@ def main():
         else:
             names.append(kc_psap.iloc[n].DsplayName)
             polys.append([kc_psap.iloc[n].geometry])
+            es_nguid.append(kc_psap.iloc[n].ES_NGUID)
 
     # Takes the lists of polygons, and merges them into new polygons for the creation of the merged_kc_psap GeoDataFrame
     merged_polys = []
@@ -63,7 +65,7 @@ def main():
         merged_polys.append(unary_union(polys[m]))
 
     # Create a new GeoDataFrame with the unique names and merged geometries
-    merged_kc_psap = gpd.GeoDataFrame({'DisplayName': names, 'geometry': merged_polys}, crs=kc_psap.crs)
+    merged_kc_psap = gpd.GeoDataFrame({'DisplayName': names, 'geometry': merged_polys, 'ES_NGUID': es_nguid}, crs=kc_psap.crs)
 
     # Find the area of the smallest merged psap, use that to determine the square size
     areas = merged_kc_psap.area
@@ -94,7 +96,7 @@ def main():
         pt = kc_ems.iloc[w].geometry.representative_point()
         rep_point = str(pt.x) + ", " + str(pt.y)
 
-        G.add_node(kc_ems.iloc[w].ES_NGUID, objectID=kc_ems.iloc[w].OBJECTID, name=kc_ems.iloc[w].DsplayName,
+        G.add_node(kc_ems.iloc[w].ES_NGUID, objectID=kc_ems.iloc[w].ES_NGUID, name=kc_ems.iloc[w].DsplayName,
                    type="EMS", point=rep_point)
 
     # Loop through all Law boundaries, adding nodes with representative points
@@ -102,7 +104,7 @@ def main():
         # Find a representative point for the boundary, and convert to string form
         pt = kc_law.iloc[v].geometry.representative_point()
         rep_point = str(pt.x) + ", " + str(pt.y)
-        G.add_node(kc_law.iloc[v].ES_NGUID, objectID=kc_law.iloc[v].OBJECTID, name=kc_law.iloc[v].DsplayName,
+        G.add_node(kc_law.iloc[v].ES_NGUID, objectID=kc_law.iloc[v].ES_NGUID, name=kc_law.iloc[v].DsplayName,
                    type="LAW", point=rep_point)
 
     # Loop through all Fire boundaries, adding nodes with representative points
@@ -110,14 +112,13 @@ def main():
         # Find a representative point for the boundary, and convert to string form
         pt = kc_fire.iloc[u].geometry.representative_point()
         rep_point = str(pt.x) + ", " + str(pt.y)
-        G.add_node(kc_fire.iloc[u].ES_NGUID, objectID=kc_fire.iloc[u].OBJECTID, name=kc_fire.iloc[u].DsplayName,
+        G.add_node(kc_fire.iloc[u].ES_NGUID, objectID=kc_fire.iloc[u].ES_NGUID, name=kc_fire.iloc[u].DsplayName,
                    type="FIRE", point=rep_point)
-
 
     # Loop through all PSAPs, adding each as nodes, and checking against all EMS, Fire, Law boundaries
     for x in range(merged_kc_psap.shape[0]):
         # add PSAP node to graph
-        G.add_node(merged_kc_psap.iloc[x].DisplayName, type="PSAP")
+        G.add_node(merged_kc_psap.iloc[x].ES_NGUID, objectID=merged_kc_psap.iloc[x].ES_NGUID, name=merged_kc_psap.iloc[x].DisplayName, type="PSAP")
 
         # find first
         # makes sure that every PSAP has at least 1 call region square
@@ -129,34 +130,32 @@ def main():
         square_counter += 1
         grid = grid.drop(contain_mask[contain_mask].index)
 
-
-
         # adding in all "adjacent PSAP" edges
         for y in range(merged_kc_psap.shape[0]):
             # finds all PSAPs that share a border with the current PSAP, and adds in adjacency edges
             if x != y and merged_kc_psap.iloc[x].geometry.touches(merged_kc_psap.iloc[y].geometry):
-                G.add_edge(merged_kc_psap.iloc[x].DisplayName, merged_kc_psap.iloc[y].DisplayName)
+                G.add_edge(merged_kc_psap.iloc[x].ES_NGUID, merged_kc_psap.iloc[y].ES_NGUID)
 
         # Loop through all EMS boundaries adding edges to relevant PSAP nodes
         for z in range(kc_ems.shape[0]):
             # Add edges between EMS and PSAP node for boundaries that overlap in any way
             if (kc_ems.iloc[z].geometry.intersects(merged_kc_psap.iloc[x].geometry) and not kc_ems.iloc[z].geometry.touches(
                     merged_kc_psap.iloc[x].geometry)) or kc_ems.iloc[z].geometry.within(merged_kc_psap.iloc[x].geometry):
-                G.add_edge(merged_kc_psap.iloc[x].DisplayName, kc_ems.iloc[z].ES_NGUID)
+                G.add_edge(merged_kc_psap.iloc[x].ES_NGUID, kc_ems.iloc[z].ES_NGUID)
 
         # Loop through all Law boundaries adding edges to relevant PSAP nodes
         for a in range(kc_law.shape[0]):
             # Add edges between Law and PSAP node for boundaries that overlap in any way
             if (kc_law.iloc[a].geometry.intersects(merged_kc_psap.iloc[x].geometry) and not kc_law.iloc[a].geometry.touches(
                     merged_kc_psap.iloc[x].geometry)) or kc_law.iloc[a].geometry.within(merged_kc_psap.iloc[x].geometry):
-                G.add_edge(merged_kc_psap.iloc[x].DisplayName, kc_law.iloc[a].ES_NGUID)
+                G.add_edge(merged_kc_psap.iloc[x].ES_NGUID, kc_law.iloc[a].ES_NGUID)
 
         # Loop through all Fire boundaries adding edges to relevant PSAP nodes
         for b in range(kc_fire.shape[0]):
             # Add edges between Fire and PSAP node for boundaries that overlap in any way
             if (kc_fire.iloc[b].geometry.intersects(merged_kc_psap.iloc[x].geometry) and not kc_fire.iloc[b].geometry.touches(
                     merged_kc_psap.iloc[x].geometry)) or kc_fire.iloc[b].geometry.within(merged_kc_psap.iloc[x].geometry):
-                G.add_edge(merged_kc_psap.iloc[x].DisplayName, kc_fire.iloc[b].ES_NGUID)
+                G.add_edge(merged_kc_psap.iloc[x].ES_NGUID, kc_fire.iloc[b].ES_NGUID)
 
     # Loop through all the squares in our grid, checking if each is completely within, or intersects with any psap
     # boundary
@@ -185,13 +184,19 @@ def main():
 
     # Creates one caller region node for each psap, and sets the appropriate region_string as an attribute.
     for n in range(merged_kc_psap.shape[0]):
-        G.add_node(str(merged_kc_psap.iloc[n].DisplayName) + " Caller region", type="Caller region", squares = str(region_string[n]))
-        G.add_edge(str(merged_kc_psap.iloc[n].DisplayName) + " Caller region", str(merged_kc_psap.iloc[n].DisplayName))
+        region_name = str(merged_kc_psap.iloc[n].DisplayName) + " Caller region"
+        caller_region_id = str(merged_kc_psap.iloc[n].ES_NGUID) + "_CR"
+        G.add_node(caller_region_id, objectID=caller_region_id, name=region_name, type="CR", squares = str(region_string[n]))
+        G.add_edge(caller_region_id, str(merged_kc_psap.iloc[n].ES_NGUID))
 
     # Print out graph information and produce .gexf file representing graph
     print("Number of nodes in graph:", G.number_of_nodes())
     print("Number of edges in graph:", G.number_of_edges())
     print("Number of square in grid: ", square_counter)
+    # We will made the vertex ids consecutive set of integers starting at zero.
+    # This is what happens when loaded into a Boost Graph Library graph and we will
+    # have a direct map between vertex ids.
+    G = nx.convert_node_labels_to_integers(G, first_label=0, ordering='default')
     nx.write_gexf(G, "graph_files/" + out_file_name + ".gexf")
     nx.write_graphml(G, "graph_files/" + out_file_name + ".graphml")
 
