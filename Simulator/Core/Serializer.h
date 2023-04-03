4d1540b1bacaacfcacde61b492b3594fd0f4e360
Divya Kamath <dkamath@otachi.uwb.edu>
2023-04-02 17:30:06 -0700

    Fix the code style voilation

--- Simulator/Core/Serializer.h
+++ Simulator/Core/Serializer.h
@@ -2,32 +2,36 @@
  * @file Serializer.h
  *
  * @ingroup Simulator/Core
- * 
- * @brief Handles implementation of serialization and deserialization of synapses.
- * 
- *  Serializes and Deserializes synapse weights, source vertices, destination vertices,
- *  maxEdgesPerVertex, totalVertices and radii.
+ *
+ * @brief Handles implementation of serialization and deserialization of
+ * synapses.
+ *
+ *  Serializes and Deserializes synapse weights, source vertices, destination
+ * vertices, maxEdgesPerVertex, totalVertices and radii.
  */
 
 class Serializer {
 public:
-   Serializer() = default;
+  Serializer() = default;
 
-   Serializer(const Serializer &serializer) = default;   // default copy constructor
-   Serializer &operator=(const Serializer &serializer) = default;   // default copy assignment
+  Serializer(const Serializer &serializer) =
+      default; // default copy constructor
+  Serializer &
+  operator=(const Serializer &serializer) = default; // default copy assignment
 
-   Serializer(Serializer &&serializer) = default;   // default move constructor
-   Serializer &operator=(Serializer &&) = default;   // default move assignment
+  Serializer(Serializer &&serializer) = default;  // default move constructor
+  Serializer &operator=(Serializer &&) = default; // default move assignment
 
-   ///  Serializes synapse weights, source vertices, destination vertices,
-   ///  maxEdgesPerVertex, totalVertices.
-   ///  if running a connGrowth model serializes radii as well
-   void serializeSynapses();
+  ///  Serializes synapse weights, source vertices, destination vertices,
+  ///  maxEdgesPerVertex, totalVertices.
+  ///  if running a connGrowth model serializes radii as well
+  void serializeSynapses();
 
-   ///  Deserializes synapse weights, source vertices, destination vertices,
-   ///  maxEdgesPerVertex, totalVertices.
-   ///  if running a connGrowth model and radii is in serialization file, deserializes radii as well
-   ///
-   ///  @returns    true if successful, false otherwise.
-   bool deserializeSynapses();
+  ///  Deserializes synapse weights, source vertices, destination vertices,
+  ///  maxEdgesPerVertex, totalVertices.
+  ///  if running a connGrowth model and radii is in serialization file,
+  ///  deserializes radii as well
+  ///
+  ///  @returns    true if successful, false otherwise.
+  bool deserializeSynapses();
 };
