a5deb1c65482dfbb9d77124460000a32d6a007e2
Divya Kamath <53545471+KamathScience@users.noreply.github.com>
2023-06-13 16:50:52 -0700

    Fix style issue in AllLIFNeurons.h

--- Simulator/Vertices/Neuro/AllLIFNeurons.h
+++ Simulator/Vertices/Neuro/AllLIFNeurons.h
@@ -109,7 +109,7 @@ public:
    ///  @param  edgeIndexMapDevice  GPU address of the EdgeIndexMap on device memory.
    virtual void advanceVertices(AllEdges &synapses, void *allVerticesDevice, void *allEdgesDevice,
                                 float randNoise[], EdgeIndexMapDevice *edgeIndexMapDevice) override;
-#else    // !defined(USE_GPU)
+#else   // !defined(USE_GPU)
 protected:
    ///  Helper for #advanceNeuron. Updates state of a single neuron.
    ///
