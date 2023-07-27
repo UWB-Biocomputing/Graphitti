0f4a3063cba1c86304b8952597ceac627db5a491
Andrzej Dawiec <andrzej5@uw.edu>
2023-07-27 14:34:19 -0700

    test

--- Simulator/Edges/Neuro/AllNeuroEdges.cpp
+++ Simulator/Edges/Neuro/AllNeuroEdges.cpp
@@ -90,14 +90,11 @@ int AllNeuroEdges::edgSign(const edgeType type)
       case EE:
          return 1;
       case ETYPE_UNDEF:
+         // TODO error.
          return 0;
-      //default:
-        // return 0;
    }
 
-   return 0; 
-   // if return 0 -> throw exception
-   // TODO Throw exception if 0 (error)
+   return 0;
 }
 
 ///  Prints SynapsesProps data to console.
