57055cc030285d3322470aa7edf2cd0909692d51
Oelgazz <125625083+Oelgazz@users.noreply.github.com>
2024-03-27 16:58:04 -0700

    Delete Tools/xml2graph.py

--- Tools/xml2graph.py
+++
@@ -1,8 +0,0 @@
-import networkx as nx
-G = nx.DiGraph()
-height = 100
-G.add_nodes_from([i for i in range(height * height)])
-for id, node in G.nodes(data=True):
-        node['x'] = id % height
-        node['y'] = id // height
-nx.write_graphml(G, 'test_graph.graphml', named_key_ids=True)
