5afcae70fc7268b165bddd90fb8df565c50503d0
Oelgazz <125625083+Oelgazz@users.noreply.github.com>
2024-05-24 12:26:10 -0700

    Update LayoutNeuro.cpp

--- Simulator/Layouts/Neuro/LayoutNeuro.cpp
+++ Simulator/Layouts/Neuro/LayoutNeuro.cpp
@@ -8,29 +8,29 @@
 
 #include "LayoutNeuro.h"
 #include "ConnGrowth.h"
-#include "ParameterManager.h"
 #include "GraphManager.h"
+#include "ParameterManager.h"
 #include "ParseParamError.h"
 #include "Util.h"
 
 // TODO: I don't think that either of the constructor or destructor is needed here
-LayoutNeuro::LayoutNeuro() : Layout()
+LayoutNeuro::LayoutNeuro() : gridLayout_(true), Layout()
 {
 }
 
 // Register vertex properties with the GraphManager
 void LayoutNeuro::registerGraphProperties()
 {
-    // The base class registers properties that are common to all vertices
-    // TODO: Currently not fully implemented because Neuro model is still integrating graphML
-    Layout::registerGraphProperties();
+   // The base class registers properties that are common to all vertices
+   // TODO: Currently not fully implemented because Neuro model is still integrating graphML
+   Layout::registerGraphProperties();
 
-    // We must register the graph properties before loading it.
-    // We are passing a pointer to a data member of the VertexProperty
-    // so Boost Graph Library can use it for loading the graphML file.
-    // Look at: https://www.studytonight.com/cpp/pointer-to-members.php
-    GraphManager& gm = GraphManager::getInstance();
-    gm.registerProperty("active", &VertexProperty::active);
+   // We must register the graph properties before loading it.
+   // We are passing a pointer to a data member of the VertexProperty
+   // so Boost Graph Library can use it for loading the graphML file.
+   // Look at: https://www.studytonight.com/cpp/pointer-to-members.php
+   GraphManager &gm = GraphManager::getInstance();
+   gm.registerProperty("active", &VertexProperty::active);
 }
 
 void LayoutNeuro::setup()
@@ -79,18 +79,18 @@ void LayoutNeuro::generateVertexTypeMap(int numVertices)
 
    // Set Neuron Type from GraphML File
    GraphManager::VertexIterator vi, vi_end;
-   GraphManager& gm = GraphManager::getInstance();
+   GraphManager &gm = GraphManager::getInstance();
 
    for (boost::tie(vi, vi_end) = gm.vertices(); vi != vi_end; ++vi) {
-       assert(*vi < numVertices_);
-       if (gm[*vi].type == "INH") {
-           vertexTypeMap_[*vi] = INH;
-           numInhibitoryNeurons++;
-       }
-       // Default Type is Excitatory
-       else {
-           vertexTypeMap_[*vi] = EXC;
-       }
+      assert(*vi < numVertices_);
+      if (gm[*vi].type == "INH") {
+         vertexTypeMap_[*vi] = INH;
+         numInhibitoryNeurons++;
+      }
+      // Default Type is Excitatory
+      else {
+         vertexTypeMap_[*vi] = EXC;
+      }
    }
 
    numExcititoryNeurons = numVertices - numInhibitoryNeurons;
@@ -107,19 +107,19 @@ void LayoutNeuro::generateVertexTypeMap(int numVertices)
 ///  Selects \e numStarter excitory neurons and converts them into starter neurons.
 ///  @param  numVertices number of vertices to have in the map.
 void LayoutNeuro::initStarterMap(int numVertices)
-{   
+{
    Layout::initStarterMap(numVertices);
-   
+
    // Set Neuron Activity from GraphML File
    GraphManager::VertexIterator vi, vi_end;
-   GraphManager& gm = GraphManager::getInstance();
+   GraphManager &gm = GraphManager::getInstance();
 
    for (boost::tie(vi, vi_end) = gm.vertices(); vi != vi_end; ++vi) {
-       assert(*vi < numVertices_);
-       if (gm[*vi].active) {
-           starterMap_[*vi] = true;
-           numEndogenouslyActiveNeurons_++;
-       }
+      assert(*vi < numVertices_);
+      if (gm[*vi].active) {
+         starterMap_[*vi] = true;
+         numEndogenouslyActiveNeurons_++;
+      }
    }
 }
 
@@ -151,22 +151,21 @@ edgeType LayoutNeuro::edgType(int srcVertex, int destVertex)
 /// Initialize the location maps (xloc and yloc).
 void LayoutNeuro::initVerticesLocs()
 {
-    // Loop over all vertices and set their x and y locations
+   // Loop over all vertices and set their x and y locations
    GraphManager::VertexIterator vi, vi_end;
 
    GraphManager &gm = GraphManager::getInstance();
-   
+
    for (boost::tie(vi, vi_end) = gm.vertices(); vi != vi_end; ++vi) {
       assert(*vi < numVertices_);
       xloc_[*vi] = gm[*vi].x;
       yloc_[*vi] = gm[*vi].y;
    }
-   
 }
 
 // Note: This code was previously used for debugging, but it is now dead code left behind
 // and it is never executed.
-/*void LayoutNeuro::printLayout()
+void LayoutNeuro::printLayout()
 {
    ConnGrowth &pConnGrowth
       = dynamic_cast<ConnGrowth &>(Simulator::getInstance().getModel().getConnections());
@@ -212,4 +211,4 @@ void LayoutNeuro::initVerticesLocs()
       ss << endl;
       cout << ss.str();
    }
-}*/
+}
