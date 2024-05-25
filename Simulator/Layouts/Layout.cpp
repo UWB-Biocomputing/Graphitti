5e6c2a3d6e32e48a09509baee55941ae66c1441f
Oelgazz <125625083+Oelgazz@users.noreply.github.com>
2024-05-25 16:33:09 -0700

    Move code from sub-classes to Layout.cpp
    The location initialization and distance calculation is now common between both Layout911.cpp and LayoutNeuro.cpp, so it is moved here.

--- Simulator/Layouts/Layout.cpp
+++ Simulator/Layouts/Layout.cpp
@@ -87,6 +87,21 @@ void Layout::setup()
       yloc_[*vi] = gm[*vi].y;
    }
 
+   // Now we cache the between each pair of vertices distances^2 into a matrix
+   for (int n = 0; n < numVertices_ - 1; n++) {
+      for (int n2 = n + 1; n2 < numVertices_; n2++) {
+         // distance^2 between two points in point-slope form
+         dist2_(n, n2) = (xloc_[n] - xloc_[n2]) * (xloc_[n] - xloc_[n2])
+                         + (yloc_[n] - yloc_[n2]) * (yloc_[n] - yloc_[n2]);
+
+         // both points are equidistant from each other
+         dist2_(n2, n) = dist2_(n, n2);
+      }
+   }
+
+   // Finally take the square root to get the distances
+   dist_ = sqrt(dist2_);
+
    // Register variable: vertex locations if need
    // Recorder &recorder = Simulator::getInstance().getModel().getRecorder();
    // string baseName = "Location";
