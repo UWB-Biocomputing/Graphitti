fc1cd76f0e6ac4668a1d5c8ca3e75d8409a37ab7
Haripriya Dhanasekaran <hdhanu@raiju.uwb.edu>
2024-12-03 12:54:02 -0800

    Added Unit tests for EventBuffer

--- Testing/UnitTesting/EventBufferTests.cpp
+++ Testing/UnitTesting/EventBufferTests.cpp
@@ -6,47 +6,49 @@
  * @ingroup Testing/UnitTesting
  */
 
-#include "gtest/gtest.h"
 #include "EventBuffer.h"
+#include "gtest/gtest.h"
 
 // A buffer which can hold 5 elements
 EventBuffer buffer(5);
 
 //GetElement when buffer is empty
-//Assuming getElement() returns uint64_t 
+//Assuming getElement() returns uint64_t
 TEST(EventBufferTest, GetElementFromEmptyBuffer)
 {
-    EXPECT_EQ(std::get<uint64_t>(buffer.getElement(0)), std::numeric_limits<unsigned long>::max());
+   EXPECT_EQ(std::get<uint64_t>(buffer.getElement(0)), std::numeric_limits<unsigned long>::max());
 }
 
 // GetPastEvent when buffer is empty
 TEST(EventBufferTest, GetPastEventFromEmptyBuffer)
 {
-    EXPECT_EQ(buffer.getPastEvent(-1), std::numeric_limits<unsigned long>::max());
+   EXPECT_EQ(buffer.getPastEvent(-1), std::numeric_limits<unsigned long>::max());
 }
 
 // Insert into empty buffer
-TEST(EventBufferTest, InsertEventEmptyBuffer) {
-    buffer.insertEvent(10);
-    buffer.insertEvent(20);
+TEST(EventBufferTest, InsertEventEmptyBuffer)
+{
+   buffer.insertEvent(10);
+   buffer.insertEvent(20);
 
-    EXPECT_EQ(std::get<uint64_t>(buffer.getElement(0)), 10);
-    EXPECT_EQ(std::get<uint64_t>(buffer.getElement(1)), 20);
+   EXPECT_EQ(std::get<uint64_t>(buffer.getElement(0)), 10);
+   EXPECT_EQ(std::get<uint64_t>(buffer.getElement(1)), 20);
 }
 
 // Insert when buffer is full, test wrap around
-TEST(EventBufferTest, BufferWrapAround) {
-    buffer.insertEvent(30);
-    buffer.insertEvent(40);
-    buffer.insertEvent(50);
+TEST(EventBufferTest, BufferWrapAround)
+{
+   buffer.insertEvent(30);
+   buffer.insertEvent(40);
+   buffer.insertEvent(50);
 
-    //Insert into A full buffer
-    //buffer.insertEvent(60);
+   //Insert into A full buffer
+   //buffer.insertEvent(60);
 
-    // The buffer should have overwritten 60 inplace of 10
-    //EXPECT_EQ(std::get<uint64_t>(buffer.getElement(0)), 60);
-    EXPECT_EQ(std::get<uint64_t>(buffer.getElement(1)), 20);
-    EXPECT_EQ(std::get<uint64_t>(buffer.getElement(2)), 30);
-    EXPECT_EQ(std::get<uint64_t>(buffer.getElement(3)), 40);
-    EXPECT_EQ(std::get<uint64_t>(buffer.getElement(4)), 50);
+   // The buffer should have overwritten 60 inplace of 10
+   //EXPECT_EQ(std::get<uint64_t>(buffer.getElement(0)), 60);
+   EXPECT_EQ(std::get<uint64_t>(buffer.getElement(1)), 20);
+   EXPECT_EQ(std::get<uint64_t>(buffer.getElement(2)), 30);
+   EXPECT_EQ(std::get<uint64_t>(buffer.getElement(3)), 40);
+   EXPECT_EQ(std::get<uint64_t>(buffer.getElement(4)), 50);
 }
